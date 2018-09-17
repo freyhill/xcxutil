b5046e9459722ec98110584b9b6be5dd98566541
leinov <1425795233@qq.com>
2018-09-17 15:54:33 +0800

    add function

--- README.md
+++ README.md
@@ -18,3 +18,14 @@ let xcxutil = require("xcxutil");
 
 xcxutil.isArray([1,2,3]); //true
 ```
+#### api
+
+api | 用途 | 实例
+---|---|---
+isArray |  是否为数组 | ```xcxutil.isArray([1,2,4])```
+trim | 去掉空格|```xcxutil.trim(" xx ")```
+getQueryString | 获取参数| ```xcxutil.getQueryString("name")```
+isObjEmpty| 检查对象是否为空|```xcxutil.isObjEmpty({}) //true```
+nowDate| 当前时间 | ```xcxutil.nowDate("-") // 2018-09-17```
+modal| 弹出框|```xcxutil.modal("这是一个提示") ```
+toast| 弹出层|```toast("这是一个提示").then(()=>{console.log("回首页")})```
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var toString = Object.prototype.toString;
 
 /**
- * 判断一个值是否为数组
+ * [判断一个值是否为数组]
  *
  * @param {Object} val 测试的值
  * @returns {boolean} 如果是数组返回true，如果非数组返回false
@@ -11,7 +11,7 @@ function isArray(val) {
   return toString.call(val) === '[object Array]';
 }
 /**
- * 去掉空格
+ * [去掉空格]
  *
  * @param {String} str 带有空格的字符串
  * @returns {String} 去掉空格的字符串
@@ -21,7 +21,7 @@ function trim(str) {
 }
 
 /**
- * 获取url参数值
+ * [获取url参数值]
  *
  * @param {String}   参数key
  * @returns {String} 返回值value
@@ -38,7 +38,7 @@ function getQueryString(name){
 }
 
 /**
- * 是否为空对象
+ * [是否为空对象]
  *
  * @param {Obj}   参数为对象
  * @returns {boolean} 返回值为布尔值
@@ -53,9 +53,115 @@ function isObjEmpty(obj){
 	return true;
 }
 
+/**
+ * [检查是否为电话号码]
+ *
+ * @param {Number} //8
+ * @returns {Boolean} // true
+ */
+function isPhone(phone) {
+  const reg = /^[1][3,4,5,7,8][0-9]{9}$/;
+  if(!reg.test(phone)) {
+    return false;
+  }
+  return true;
+}
+
+/**
+   * [获取两个数组的交集]
+   *
+   * @param {Array} //[1,2,3,4,5,8,9]
+   * @param {Array} //[1,11,9]
+   * @returns {Array} // [1,9]
+   */
+  function interArray(arr1, arr2, key) {
+    let commonarr = arr1.filter((x) => {
+      if(key) {
+        return arr2.includes(x[key])
+      } else {
+        return arr2.includes(x)
+      }
+    })
+    return commonarr;
+  }
+/**
+ * [获取当前的日期]
+ *
+ * @param {string} '-'
+ * @returns {string}  '2018-09-16'
+ */
+function nowDate(type = '-') {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = datePlus0(new Date().getMonth() + 1);
+  const currentDate = datePlus0(new Date().getDate());
+  return `${year}${type}${month}${type}${currentDate}`;
+}
+
+/**
+   * [补0]
+   *
+   * @param {Number} //8
+   * @returns {String} // 08
+   */
+function datePlus0(x) {
+  if(x < 10) {
+    return `0${x}`;
+  } else {
+    return x
+  }
+}
+/**
+ * [弹出确认窗口]
+ * @param {text} //8
+ * @param {title} // 08
+ */
+function modal(text, title = '提示') {
+  return new Promise((resolve, reject) => {
+    wx.showModal({
+      title: title,
+      content: text,
+      showCancel: false,
+      success: res => {
+        resolve(res);
+      },
+      fail: res => {
+        reject(res);
+      }
+    });
+  });
+}
+
+/**
+ * [提示弹层]
+ * @param {title} // 显示内容
+ * @param {duration} // 显示时间
+ * @param {icon} // 图标
+ */
+ function toast(title, duration=3000, icon = 'success') {
+   return new Promise((resolve,reject)=>{
+     wx.showToast({
+       title: title,
+       icon: icon,
+       mask: true,
+       duration: duration,
+       success:function(){
+         setTimeout(()=>{
+           resolve();
+         },duration)
+       }
+     });
+   })
+ }
+
 module.exports = {
   isArray: isArray,
   trim:trim,
   getQueryString:getQueryString,
-  isObjEmpty:isObjEmpty
+  isObjEmpty:isObjEmpty,
+  isPhone:isPhone,
+  interArray:interArray,
+  nowDate:nowDate,
+  modal:modal,
+  toast:toast
 };
